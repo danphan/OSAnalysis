10ae1eda899a69e2e29ef43f75a4bedcf1473db6
Dan Phan <dan@umail.ucsb.edu>
2015-02-06 19:42:38 -0800

    do file for allplot.C added

---
+++ OSAnalysis/doallplot.C
@@ -0,0 +1,12 @@
+{
+
+  gSystem->Load("~/CORE/libCMS2NtupleMacrosCORE.so");
+  //gROOT->Load("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-23/merged_ntuple_100.root");
+  //gROOT->Load("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DYJetsToLL_M-10To50filter_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-13_slim/merged_ntuple_100.root");
+  gROOT->ProcessLine(".L /home/users/yanjunhe/dataMCplotMaker/dataMCplotMaker.cc+");
+ // gROOT->ProcessLine(".L /home/users/yanjunhe/CORE/CMS2.cc++");
+  gROOT->ProcessLine(".L SS.cc++");
+  gROOT->ProcessLine(".L allplot.C++");
+  gROOT->ProcessLine("allplot()");
+  //gROOT->ProcessLine(".exception");
+}
