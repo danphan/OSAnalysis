9f921e6b682790f9e0ac67fbe5b7afc56bb02b0d
JackHe1412 <yanjun_he@umail.ucsb.edu>
2015-02-06 22:25:18 -0800

    Update doallplot.C

--- OSAnalysis/doallplot.C
+++ OSAnalysis/doallplot.C
@@ -1,6 +1,6 @@
 {
 
-  gSystem->Load("~/CORE/libCMS2NtupleMacrosCORE.so");
+  gSystem->Load("/home/users/yanjunhe/CORE/libCMS2NtupleMacrosCORE.so");
   gROOT->ProcessLine(".L /home/users/yanjunhe/dataMCplotMaker/dataMCplotMaker.cc+");
   gROOT->ProcessLine(".L SS.cc+");
   gROOT->ProcessLine(".L allplot.C+");
