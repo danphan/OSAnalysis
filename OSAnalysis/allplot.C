6f57f4ce648ce9ba717ef2260af658c51c65f048
JackHe1412 <yanjun_he@umail.ucsb.edu>
2015-02-06 22:37:51 -0800

    Update allplot.C

--- OSAnalysis/allplot.C
+++ OSAnalysis/allplot.C
@@ -8,16 +8,16 @@
 #include "TTree.h"
 #include "Math/VectorUtil.h"
 
+using namespace Jack;
+
 int allplot(){
  
-
   TFile *file = new TFile("/home/users/yanjunhe/baby.root");
 
 
   TTree *tree = (TTree*)file->Get("tree");
 
-
-  //lumi and corr for these events
+ //lumi and corr for these events
 
   float lumi = 19.5;
 
@@ -33,13 +33,6 @@ int allplot(){
   TH1F *hist_mass1 =  new TH1F("hist_mass","mass Z",50,0,150);
   TH1F *hist_phi1 = new TH1F("hist_phiZ","phi Z", 50, -3.5, 3.5);
   TH1F *hist_numjet = new TH1F("hist_numjet","num jets",8,0,8);
-//  TH1F *hist_e = new TH1F("hist_e","num electron",5,0,5);
-//  TH1F *hist_m = new TH1F("hist_m","num muon",5,0,5);
-//  TH1F *hist_eb = new TH1F("hist_eb","num before electron",5,0,5);
-//  TH1F *hist_mb = new TH1F("hist_mb","num before muon",5,0,5);
-  
-
-
 
   cout << tree->GetEntries() << endl;
   
@@ -50,57 +43,29 @@ int allplot(){
   int dummy = 0;
 
   for(unsigned int evt = 0; evt < nEventsTree; evt++) { 
- //  int nj = 0; 
- //  int e = 0;
- //  int eb = 0;
- //  int m = 0;
- //  int mb = 0;  
+ 
     Object.GetEntry(evt);
    
     //define leptons
-    LorentzVector lep1 = Jack::lep1_p4(); 
-    LorentzVector lep2 = Jack::lep2_p4(); 
+    LorentzVector lep1 = lep1_p4(); 
+    LorentzVector lep2 = lep2_p4(); 
   
-
-    //this dummy variable seems superfluous 
-    dummy = 1;   
-    
     LorentzVector z_cand = lep1 + lep2;
  
     //fill plots with information about dileptons
-    hist_pt->Fill(lep1.pt(),Jack::evt_scale1fb()*lumi*corr);   //Jack::evt_scale1fb()*lumi*corr
-    hist_pt->Fill(lep2.pt(),Jack::evt_scale1fb()*lumi*corr);
-    hist_eta->Fill(lep1.eta(),Jack::evt_scale1fb()*lumi*corr);
-    hist_eta->Fill(lep2.eta(),Jack::evt_scale1fb()*lumi*corr);
-    hist_phi->Fill(lep1.phi(),Jack::evt_scale1fb()*lumi*corr);
-    hist_phi->Fill(lep2.phi(),Jack::evt_scale1fb()*lumi*corr);
-    hist_pt1->Fill(z_cand.pt(),Jack::evt_scale1fb()*lumi*corr);
-    hist_eta1->Fill(z_cand.eta(),Jack::evt_scale1fb()*lumi*corr);
-    hist_phi1->Fill(z_cand.phi(),Jack::evt_scale1fb()*lumi*corr);
-    hist_mass1->Fill(z_cand.mass(),Jack::evt_scale1fb()*lumi*corr); 
-
-           
- // for  (unsigned int q = 0; q < Jack::pfjets_p4().size();q++) {
- // 
- //   LorentzVector jet2 = Jack::pfjets_p4().at(q);     
- //   
- //   bool uu = 1;
- //   
- //   if (jet2.pt()<=30 || abs(jet2.eta())>=3 ) continue;
- //   
- //   for(unsigned int w = 0; w < lepton_cands2.size(); w++) {
- //     if (ROOT::Math::VectorUtil::DeltaR(lepton_cands2.at(w),jet2) < 0.1) {
- //       uu = 0;
- //       break;
- //     }
- //   }  
- //   if (uu == 1) nj++;
- // }
-   
-    if (dummy == 1) {
-      hist->Fill(Jack::evt_pfmet(),Jack::evt_scale1fb()*lumi*corr);
-      hist_numjet->Fill(Jack::njets(),Jack::evt_scale1fb()*lumi*corr);
-    }
+    hist_pt->Fill(lep1.pt(), scale1fb()*lumi*corr);   //Jack::evt_scale1fb()*lumi*corr
+    hist_pt->Fill(lep2.pt(), scale1fb()*lumi*corr);
+    hist_eta->Fill(lep1.eta(), scale1fb()*lumi*corr);
+    hist_eta->Fill(lep2.eta(), scale1fb()*lumi*corr);
+    hist_phi->Fill(lep1.phi(), scale1fb()*lumi*corr);
+    hist_phi->Fill(lep2.phi(), scale1fb()*lumi*corr);
+    hist_pt1->Fill(z_cand.pt(), scale1fb()*lumi*corr);
+    hist_eta1->Fill(z_cand.eta(), scale1fb()*lumi*corr);
+    hist_phi1->Fill(z_cand.phi(), scale1fb()*lumi*corr);
+    hist_mass1->Fill(z_cand.mass(), scale1fb()*lumi*corr); 
+    hist->Fill(Jack::evt_pfmet(), scale1fb()*lumi*corr);
+    hist_numjet->Fill(Jack::njets(), scale1fb()*lumi*corr);
+    
   }
  
   //using dataMCplotMaker to plot the hists made 
@@ -109,14 +74,12 @@ int allplot(){
  
   vector <TH1F*> vec1_hist,vec2_hist,vec3_hist,vec4_hist,vec5_hist,vec6_hist,vec7_hist,vec_hist,vec8_hist;//,vec9_hist,vec10_hist,vec_hist;
 
-
   vector <char*> vec1_titles,vec2_titles,vec3_titles,vec4_titles,vec5_titles,vec6_titles,vec7_titles,vec_titles,vec8_titles;//,vec9_titles,vec10_titles;
  
   vec_hist.push_back(hist); 
   vec_titles.push_back("Z #rightarrow l^{+} l^{-}");
   dataMCplotMaker (null,vec_hist,vec_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--legendRight -0.05 --isLinear --outputName met --xAxisLabel MET");
  
-
   vec1_hist.push_back(hist_pt);
   vec1_titles.push_back("Z #rightarrow l^{+} l^{-}");
   dataMCplotMaker (null,vec1_hist,vec1_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--legendRight -0.05 --isLinear --outputName lep_pt --xAxisLabel pt");
@@ -134,16 +97,20 @@ int allplot(){
   dataMCplotMaker (null,vec4_hist,vec4_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05 --outputName Z_pt --xAxisLabel Z_pt");
 
   vec5_hist.push_back(hist_eta1);
-  vec5_titles.push_back("Z #rightarrow l^{+} l^{-}");                              dataMCplotMaker (null,vec5_hist,vec5_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05 --noXaxisUnit  --outputName Z_eta --xAxisLabel Z_eta"); 
+  vec5_titles.push_back("Z #rightarrow l^{+} l^{-}"); 
+  dataMCplotMaker (null,vec5_hist,vec5_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05 --noXaxisUnit  --outputName Z_eta --xAxisLabel Z_eta"); 
 
   vec6_hist.push_back(hist_phi1);
-  vec6_titles.push_back("Z #rightarrow l^{+} l^{-}");                             dataMCplotMaker (null,vec6_hist,vec6_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05  --noXaxisUnit --outputName Z_phi --xAxisLabel Z_phi"); 
+  vec6_titles.push_back("Z #rightarrow l^{+} l^{-}");   
+  dataMCplotMaker (null,vec6_hist,vec6_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05  --noXaxisUnit --outputName Z_phi --xAxisLabel Z_phi"); 
 
   vec7_hist.push_back(hist_mass1);
-  vec7_titles.push_back("Z #rightarrow l^{+} l^{-}");                             dataMCplotMaker (null,vec7_hist,vec7_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05  --outputName Z_mass --xAxisLabel Z_mass"); 
+  vec7_titles.push_back("Z #rightarrow l^{+} l^{-}");  
+  dataMCplotMaker (null,vec7_hist,vec7_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05  --outputName Z_mass --xAxisLabel Z_mass"); 
 
   vec8_hist.push_back(hist_numjet);
-  vec8_titles.push_back("Z #rightarrow l^{+} l^{-}");                              dataMCplotMaker (null,vec8_hist,vec8_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05 --noXaxisUnit --xAxisLabel number of jets  --outputName numjets --noDivisionLabel"); 
+  vec8_titles.push_back("Z #rightarrow l^{+} l^{-}");   
+  dataMCplotMaker (null,vec8_hist,vec8_titles,"RUN 1 MC Exercise","#geq 1 OS,SF dilepton pairs","--isLinear --legendRight -0.05 --noXaxisUnit --xAxisLabel number of jets  --outputName numjets --noDivisionLabel"); 
 
   return 0;
 }
