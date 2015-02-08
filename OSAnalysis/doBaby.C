7dc85677842145b77a698d74eaa93c2c96b73049
JackHe1412 <yanjun_he@umail.ucsb.edu>
2015-02-08 13:04:46 -0800

    Update doBaby.C

--- OSAnalysis/doBaby.C
+++ OSAnalysis/doBaby.C
@@ -1,6 +1,6 @@
 {
   gROOT->ProcessLine(".L /home/users/yanjunhe/dataMCplotMaker/dataMCplotMaker.cc+");
-  gROOT->ProcessLine(".L /home/users/yanjunhe/SS.cc+");
+  gROOT->ProcessLine(".L SS.cc+");
   gROOT->ProcessLine(".L baby.C+");
   gROOT->ProcessLine("baby()");
 }
